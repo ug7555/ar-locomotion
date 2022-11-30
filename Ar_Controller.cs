7ab57e7b5ca98d025c4af4319939c7ac72f870a6
XX-UG-XX <113767952+XX-UG-XX@users.noreply.github.com>
2022-11-30 23:51:55 +0530

    Add files via upload

---
+++ Ar_Controller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class Ar_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject MyObject;
+    public ARRaycastManager RaycastManager;
+
+    private void update()
+    {
+        if(Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            List<ARRaycastHit> touches = new List<ARRaycastHit>();
+
+            RaycastManager.Raycast(Input.GetTouch(0).position,touches,UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+            if(touches.Count > 0)
+            {
+                GameObject.Instantiate(MyObject, touches[0].pose.position, touches[0].pose.rotation);
+            }
+            
+
+        }
+    }
+}
+
+
